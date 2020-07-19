79be5f495e74b8d4b755b413edfde64678cfdcd2
Todd Birchard <toddbirchard@gmail.com>
2020-07-19 15:21:25 -0400

    Return slice of errors.

--- main_test.go
+++ main_test.go
@@ -11,7 +11,7 @@ func TestCreateRequest(t *testing.T) {
 	cookie := "__stripe_mid=939f07db-3218-4e38-b192-4616eef64ec0; em_cdn_uid=t%3D1587926226095%26u%3Dde7c8991dea14a95b1a1c0036b9c6372; em_p_uid=l:1592880910636|t:1587926226510|u:b214ccdf85d944a9baa66aa70c9997f0; express:sess=eyJ0b2tlbiI6ImEwOWUwY2VhZDE0YTkyYmYxNzhmOWQyYTQ2NTM2MDM5OGVlZDcxNTg3YjY2Y2MyNzU4ZDZmMWJjNGFkODVmODkiLCJzYWx0IjoiMTU5NDE1ODMzNDE3OCJ9; ghost-members-ssr=toddbirchard+jfphhudgshjdiwhekjb@gmail.com; ghost-members-ssr.sig=tM3JLCh6p3msMkt_hAiQmOZl3hg; __stripe_sid=3beca9ec-39cf-42b2-9a75-b3d5fa008faf"
 	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
-		fmt.Println(err)
+		t.Fatal(err)
 	}
 	req.Header.Add("cookie", cookie)
 	fmt.Println(req)
